eb2e6e6ca8832642a171ecdea3d4f0bf6d5e9157
Luis Garcia <104cubes@gmail.com>
2025-11-19 10:38:13 +0100

    Update VerifactuXMLBuilder.class.php

--- class/VerifactuXMLBuilder.class.php
+++ class/VerifactuXMLBuilder.class.php
@@ -1,234 +1,103 @@
 <?php
 
-/**
- * Generador XML VeriFactu (RegistroAlta) + envoltura SOAP
- * Plantilla inicial para altas "normales" (sin subsanación).
- */
-
-if (!class_exists('VerifactuXMLBuilder')) {
-
-    class VerifactuXMLBuilder
+class VerifactuXMLBuilder
+{
+    /**
+     * Construye el XML SOAP + RegistroAlta con todos los datos.
+     * $d debe contener:
+     *   emisor_nif
+     *   emisor_nombre
+     *   receptor_nif
+     *   ref
+     *   fecha
+     *   tipo_factura   (F1, R1...)
+     *   descripcion
+     *   base
+     *   iva
+     *   total
+     *   hash_prev
+     *   hash_actual
+     *   timestamp (en formato ISO 8601)
+     */
+    public function buildRegistroAltaXML(array $d)
     {
-        /** @var DoliDB */
-        protected $db;
-
-        /** @var Conf */
-        protected $conf;
-
-        /**
-         * @param DoliDB $db
-         * @param Conf   $conf
-         */
-        public function __construct($db, $conf)
-        {
-            $this->db   = $db;
-            $this->conf = $conf;
+        // Sanitizar pequeños fallos (evitar nulls)
+        foreach ($d as $k => $v) {
+            if (!isset($v)) $d[$k] = '';
         }
 
-        /**
-         * Construye el XML interno de RegistroAlta (sin SOAP).
-         *
-         * Esto sigue la estructura de ejemplo de RegistroAlta de la AEAT:
-         * - Namespace SuministroInformacion.xsd (sum1)
-         * - IDVersion, IDFactura, NombreRazonEmisor, Subsanacion, RechazoPrevio, TipoFactura, etc.
-         *
-         * @param Facture $facture        Factura Dolibarr
-         * @param string  $hashPrev       Huella anterior encadenada (cadena vacía si no hay)
-         * @param string  $hashActual     Huella actual ya calculada (SHA256 en hex mayúsculas)
-         * @param int     $timestamp      Timestamp UNIX de generación del registro (dol_now())
-         * @param bool    $esSubsanacion  Si es subsanación (Subsanacion=S)
-         *
-         * @return string XML de <sum1:RegistroAlta>...</sum1:RegistroAlta>
-         */
-        public function buildRegistroAlta($facture, $hashAnterior, $hashActual, $timestamp, $isSubsanacion = false)
-        {
-            $dom = new DOMDocument('1.0', 'UTF-8');
-            $dom->formatOutput = true;
+        $xml = <<<XML
+<sum1:RegistroAlta xmlns:sum1="https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroInformacion.xsd">
+	<sum1:IDVersion>1.0</sum1:IDVersion>
+	<sum1:IDFactura>
+		<sum1:IDEmisorFactura>{$d['emisor_nif']}</sum1:IDEmisorFactura>
+		<sum1:NumSerieFactura>{$d['ref']}</sum1:NumSerieFactura>
+		<sum1:FechaExpedicionFactura>{$d['fecha']}</sum1:FechaExpedicionFactura>
+	</sum1:IDFactura>
+	<sum1:NombreRazonEmisor>{$d['emisor_nombre']}</sum1:NombreRazonEmisor>
+	<sum1:Subsanacion>N</sum1:Subsanacion>
+	<sum1:RechazoPrevio>N</sum1:RechazoPrevio>
+	<sum1:TipoFactura>F1</sum1:TipoFactura>
+	
+	<sum1:FechaOperacion>03-02-2025</sum1:FechaOperacion>
+	<sum1:DescripcionOperacion>{$d['descripcion']}</sum1:DescripcionOperacion>
+	<sum1:Destinatarios>
+		<sum1:IDDestinatario>
+			<sum1:NombreRazon></sum1:NombreRazon>
+			<sum1:NIF>{$d['receptor_nif']}</sum1:NIF>
+		</sum1:IDDestinatario>
+	</sum1:Destinatarios>
+	<sum1:Desglose>
+		<sum1:DetalleDesglose>
+			<sum1:ClaveRegimen>01</sum1:ClaveRegimen>
+			<sum1:CalificacionOperacion>S1</sum1:CalificacionOperacion>
+			<sum1:TipoImpositivo>{$d['iva']}</sum1:TipoImpositivo>
+			<sum1:BaseImponibleOimporteNoSujeto>{$d['base']}</sum1:BaseImponibleOimporteNoSujeto>
+			<sum1:CuotaRepercutida>{$d['cuota']}</sum1:CuotaRepercutida>
+		</sum1:DetalleDesglose>
+		<sum1:DetalleDesglose>
+			<sum1:ClaveRegimen>01</sum1:ClaveRegimen>
+			<sum1:CalificacionOperacion>S1</sum1:CalificacionOperacion>
+			<sum1:TipoImpositivo>{$d['iva']}</sum1:TipoImpositivo>
+			<sum1:BaseImponibleOimporteNoSujeto>{$d['base']}</sum1:BaseImponibleOimporteNoSujeto>
+			<sum1:CuotaRepercutida>{$d['cuota']}</sum1:CuotaRepercutida>
+		</sum1:DetalleDesglose>
+		<sum1:DetalleDesglose>
+			<sum1:ClaveRegimen>05</sum1:ClaveRegimen>
+			<sum1:CalificacionOperacion>S1</sum1:CalificacionOperacion>
+			<sum1:TipoImpositivo>{$d['iva']}</sum1:TipoImpositivo>
+			<sum1:BaseImponibleOimporteNoSujeto>{$d['base']}</sum1:BaseImponibleOimporteNoSujeto>
+			<sum1:CuotaRepercutida>{$d['cuota']}</sum1:CuotaRepercutida>
+		</sum1:DetalleDesglose>
+	</sum1:Desglose>
+	<sum1:CuotaTotal>{$d['cuota']}</sum1:CuotaTotal>
+	<sum1:ImporteTotal>{$d['total']}</sum1:ImporteTotal>
+	<sum1:Encadenamiento>
+		<sum1:RegistroAnterior>
+			<sum1:IDEmisorFactura>89890001K</sum1:IDEmisorFactura>
+			<sum1:NumSerieFactura>12345677-G33</sum1:NumSerieFactura>
+			<sum1:FechaExpedicionFactura>15-04-2024</sum1:FechaExpedicionFactura>
+			<sum1:Huella>{$d['hash_prev']}</sum1:Huella>
+		</sum1:RegistroAnterior>
+	</sum1:Encadenamiento>
+	<sum1:SistemaInformatico>
+		<sum1:NombreRazon>{$d['emisor_nombre']}</sum1:NombreRazon>
+		<sum1:NIF>{$d['emisor_nif']}</sum1:NIF>
+		<sum1:NombreSistemaInformatico>NombreSistemaInformatico</sum1:NombreSistemaInformatico>
+		<sum1:IdSistemaInformatico>77</sum1:IdSistemaInformatico>
+		<sum1:Version>1.0.03</sum1:Version>
+		<sum1:NumeroInstalacion>383</sum1:NumeroInstalacion>
+		<sum1:TipoUsoPosibleSoloVerifactu>S</sum1:TipoUsoPosibleSoloVerifactu>
+		<sum1:TipoUsoPosibleMultiOT>N</sum1:TipoUsoPosibleMultiOT>
+		<sum1:IndicadorMultiplesOT>N</sum1:IndicadorMultiplesOT>
+	</sum1:SistemaInformatico>
+	<sum1:FechaHoraHusoGenRegistro>{$d['timestamp']}</sum1:FechaHoraHusoGenRegistro>
+	<sum1:TipoHuella>01</sum1:TipoHuella>
+	<sum1:Huella>{$d['hash_actual']}</sum1:Huella>
+</sum1:RegistroAlta>
 
-            // Namespaces
-            $soapenv = "http://schemas.xmlsoap.org/soap/envelope/";
-            $sum1    = "https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroInformacion.xsd";
-            $lr      = "https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroLR.xsd";
+XML;
 
-            // Root (RegistroAlto Interno)
-            $root = $dom->createElementNS($sum1, "sum1:RegistroAlta");
-            $dom->appendChild($root);
-
-            // Subsanación opcional
-            if ($isSubsanacion === true) {
-                $root->appendChild($dom->createElement("sum1:Subsanacion", "S"));
-                $root->appendChild($dom->createElement("sum1:RechazoPrevio", "S"));
-            }
-
-            // Identificación de factura
-            $idf = $dom->createElement("sum1:IDFactura");
-            $root->appendChild($idf);
-
-            $idf->appendChild($dom->createElement("sum1:NumSerieFacturaEmisor", $facture->ref));
-            $fecha = dol_print_date($facture->date, '%Y-%m-%d');
-            $idf->appendChild($dom->createElement("sum1:FechaExpedicionFacturaEmisor", $fecha));
-
-            // Datos de facturación
-            $df = $dom->createElement("sum1:DatosFactura");
-            $root->appendChild($df);
-
-            $df->appendChild($dom->createElement("sum1:TipoFactura", "F1"));   // De momento F1
-            $df->appendChild($dom->createElement("sum1:DescripcionOperacion", $facture->note_public ?: 'Factura emitida mediante VeriFactu'));
-
-            // Bases imponibles (simplificado)
-            $bi = $dom->createElement("sum1:Importes");
-            $df->appendChild($bi);
-
-            $bi->appendChild($dom->createElement("sum1:BaseImponible", number_format($facture->total_ht, 2, '.', '')));
-            $bi->appendChild($dom->createElement("sum1:CuotaIVA", number_format($facture->total_tva, 2, '.', '')));
-            $bi->appendChild($dom->createElement("sum1:ImporteTotal", number_format($facture->total_ttc, 2, '.', '')));
-
-            // Encadenamiento
-            $enc = $dom->createElement("sum1:Encadenamiento");
-            $root->appendChild($enc);
-
-            $enc->appendChild($dom->createElement("sum1:TipoHuella", "01"));
-            if (!empty($hashAnterior)) {
-                $enc->appendChild($dom->createElement("sum1:HuellaAnterior", $hashAnterior));
-            }
-
-            // Hash actual de esta factura
-            $root->appendChild($dom->createElement("sum1:TipoHuella", "01"));
-            $root->appendChild($dom->createElement("sum1:Huella", $hashActual));
-
-            // Fecha/hora
-            $root->appendChild($dom->createElement("sum1:FechaHoraHuella", date('c', $timestamp)));
-
-            return $dom->saveXML();
-        }
-
-        /**
-         * Envuelve un fragmento XML (RegistroAlta / RegistroAnulacion) en el SOAP completo
-         * según el WSDL de AEAT (SuministroLR + SuministroInformacion).
-         *
-         * @param string $registroXml  XML interno del RegistroAlta o RegistroAnulacion
-         *
-         * @return string XML completo SOAP listo para enviar por cURL
-         */
-        public function buildSoapEnvelope($registroXml)
-        {
-            $soapenv_ns = 'http://schemas.xmlsoap.org/soap/envelope/';
-            $sum_ns     = 'https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroLR.xsd';
-            $sum1_ns    = 'https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroInformacion.xsd';
-
-            $dom = new DOMDocument('1.0', 'UTF-8');
-            $dom->formatOutput = true;
-
-            // Envelope
-            $envelope = $dom->createElementNS($soapenv_ns, 'soapenv:Envelope');
-            $dom->appendChild($envelope);
-
-            // Declarar namespaces sum y sum1
-            $envelope->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:sum', $sum_ns);
-            $envelope->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:sum1', $sum1_ns);
-
-            // Header vacío
-            $header = $dom->createElement('soapenv:Header');
-            $envelope->appendChild($header);
-
-            // Body
-            $body = $dom->createElement('soapenv:Body');
-            $envelope->appendChild($body);
-
-            // RegFactuSistemaFacturacion
-            $reg = $dom->createElement('sum:RegFactuSistemaFacturacion');
-            $body->appendChild($reg);
-
-            // Cabecera
-            $cabecera = $dom->createElement('sum:Cabecera');
-            $reg->appendChild($cabecera);
-
-            $cabecera->appendChild($dom->createElement('sum:IDVersion', '1.0'));
-
-            $nif_empresa    = $this->conf->global->MAIN_INFO_SIREN
-                ?: $this->conf->global->MAIN_INFO_TVAINTRA
-                ?: 'B00000000';
-            $nombre_empresa = $this->conf->global->MAIN_INFO_SOCIETE_NOM ?: 'Empresa Dolibarr';
-
-            $titular = $dom->createElement('sum:Titular');
-            $cabecera->appendChild($titular);
-            $titular->appendChild($dom->createElement('sum:NIF', $nif_empresa));
-            $titular->appendChild($dom->createElement('sum:NombreRazon', dol_escape_htmltag($nombre_empresa)));
-
-            // TipoComunicacion A0 = Alta
-            $cabecera->appendChild($dom->createElement('sum:TipoComunicacion', 'A0'));
-
-            // RegistroFactura conteniendo el RegistroAlta / RegistroAnulacion
-            $regFactura = $dom->createElement('sum:RegistroFactura');
-            $reg->appendChild($regFactura);
-
-            // Importar el fragmento interno (RegistroAlta)
-            if (!empty($registroXml)) {
-                $tmpDom = new DOMDocument('1.0', 'UTF-8');
-                $tmpDom->loadXML($registroXml);
-                $imported = $dom->importNode($tmpDom->documentElement, true);
-                $regFactura->appendChild($imported);
-            }
-
-            return $dom->saveXML();
-        }
-
-        /**
-         * Método de ayuda: construye el RegistroAlta normal, lo envuelve en SOAP
-         * y lo guarda en disco.
-         *
-         * @param Facture $facture
-         * @param string  $hashPrev
-         * @param string  $hashActual
-         * @param int     $timestamp
-         * @param string  $xmlPath   Ruta completa del fichero a guardar
-         *
-         * @return string XML SOAP final
-         */
-        public function buildAltaSoapAndSave($facture, $hashPrev, $hashActual, $timestamp, $xmlPath)
-        {
-            $xmlRegistro = $this->buildRegistroAltaXml($facture, $hashPrev, $hashActual, $timestamp, false);
-            $xmlSoap     = $this->buildSoapEnvelope($xmlRegistro);
-
-            // Guardar en disco
-            dol_mkdir(dirname($xmlPath));
-            file_put_contents($xmlPath, $xmlSoap);
-
-            return $xmlSoap;
-        }
-        public function buildRegistroAnulacion($facture, $hashAnterior, $timestamp)
-        {
-            $dom = new DOMDocument('1.0', 'UTF-8');
-            $dom->formatOutput = true;
-
-            $sum1 = "https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/tike/cont/ws/SuministroInformacion.xsd";
-
-            $root = $dom->createElementNS($sum1, "sum1:RegistroAnulacion");
-            $dom->appendChild($root);
-
-            $idf = $dom->createElement("sum1:IDFactura");
-            $root->appendChild($idf);
-
-            $idf->appendChild($dom->createElement("sum1:NumSerieFacturaEmisor", $facture->ref));
-            $idf->appendChild($dom->createElement("sum1:FechaExpedicionFacturaEmisor", dol_print_date($facture->date, '%Y-%m-%d')));
-
-            $root->appendChild($dom->createElement("sum1:MotivoAnulacion", "01")); // Error material
-
-            // Encadenamiento de anulaciones
-            $enc = $dom->createElement("sum1:Encadenamiento");
-            $root->appendChild($enc);
-
-            $enc->appendChild($dom->createElement("sum1:TipoHuella", "01"));
-            if (!empty($hashAnterior)) {
-                $enc->appendChild($dom->createElement("sum1:HuellaAnterior", $hashAnterior));
-            }
-
-            $root->appendChild($dom->createElement("sum1:TipoHuella", "01"));
-            $root->appendChild($dom->createElement("sum1:Huella", sha1($facture->ref . $timestamp)));
-
-            $root->appendChild($dom->createElement("sum1:FechaHoraHuella", date('c', $timestamp)));
-
-            return $dom->saveXML();
-        }
+        return $xml;
     }
 }
